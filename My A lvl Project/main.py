c5ae963c74147dbc1e4341c9aeb3f5419cb2e173
Elyas Chaal <b33822@sfc.potteries.ac.uk>
2024-09-08 17:21:37 +0100

    Update main.py

--- My A lvl Project/main.py
+++ My A lvl Project/main.py
@@ -1,11 +1,10 @@
 import tkinter as tk
-import gui
-#from gui import create_main_window
+from gui import create_main_window
 #from database import create_table
 
 if __name__ == "__main__":
     #create_table() # Set up a table in database
     root = tk.Tk()
     root.title("Personal Finance Tracker")
-    gui.create_main_window(root)
+    create_main_window(root)
     root.mainloop()
