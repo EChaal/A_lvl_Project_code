4f2f840d58dc8b2d2f111d72bb83751cffb1533e
Elyas Chaal <b33822@sfc.potteries.ac.uk>
2024-09-08 16:08:19 +0100

    did something

---
+++ My A lvl Project/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from gui import create_main_window
+#from database import create_table
+
+if __name__ == "__main__":
+    #create_table() # Set up a table in database
+    root = tk.Tk()
+    root.title("Personal Finance Tracker")
+    main_window.create_main_window(root)
+    root.mainloop()
